9c3a2cb7836ec0a5550fcbfced3b9978de15d9ea
TuanAnh792002 <letuananh792002@gmail.com>
2023-12-31 22:03:50 +0700

    1st commit

---
+++ main/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+#include "sdkconfig.h"
+
+
+
+#endif
+
